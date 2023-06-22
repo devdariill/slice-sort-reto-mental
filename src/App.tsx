14bca2fb06890fa77cee6023ab8b757c34e6e432
nATOr <gowluonade@gmail.com>
2023-06-22 05:26:13 -0500

    setInterval

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [status, setStatus] = useState<'initial'|'playing'|'finished'>('initial')
   const [time, setTime] = useState<number>(0)
   const [score, setScore] = useState<number>(0)
   
+  useEffect(()=>{
+    let interval: number // NodeJS.Timeout
+    if (status === 'playing') {
+      interval = setInterval(() => {
+        setTime(time => time + 1)
+      }, 1000);
+    }
+    return () => clearInterval(interval)
+  },[status])
+
   return (
     <main className='h-screen flex flex-col w-screen'>
       <header className='flex gap-x-3 text-xs  w-full justify-center'>
         <h1>{0} puntos</h1>
-        <h1>{0} segundos</h1>
+        <h1>{time} segundos</h1>
       </header>
       { status === 'playing' &&
         <section>
